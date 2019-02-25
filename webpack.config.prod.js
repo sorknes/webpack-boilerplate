2e6a03fa05ec694defd13b0dd003480e32498a4b
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-02-25 13:30:50 +0100

    Add clean plugin, build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,4 +4,19 @@
 // ===============================================================================
 //
 
-module.exports = {}
+const path = require('path'),
+  CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const merge = require('webpack-merge'),
+  common = require('./webpack.config.common.js')
+
+module.exports = merge(common, {
+  mode: 'production',
+
+  output: {
+    filename: './scripts/[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [new CleanWebpackPlugin(['dist'])],
+})

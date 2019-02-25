b66f1a466e749bf86cb9b0e90772edcd3b3930e4
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-02-25 13:27:25 +0100

    Setup nunjucks

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -4,14 +4,48 @@
 // ===============================================================================
 //
 
-let src = `./src`
+const path = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  glob_entries = require('webpack-glob-folder-entries')
+
+let src = path.resolve(__dirname, 'src')
 let styles = `${src}/styles/scss`
 let scripts = `${src}/scripts`
+
+function returnEntries(globPath) {
+  let entries = glob_entries(globPath, true)
+  let folderList = new Array()
+  for (let folder in entries) {
+    folderList.push(path.join(__dirname, entries[folder]))
+  }
+  return folderList
+}
 
 module.exports = {
   entry: [`${scripts}/main.js`, `${styles}/styles.scss`],
 
   module: {
-    rules: [{}],
+    rules: [
+      {
+        test: /\.html$|njk|nunjucks/,
+        use: [
+          'html-loader',
+          {
+            loader: 'nunjucks-html-loader',
+            options: {
+              searchPaths: [...returnEntries('./src/templates/**/')],
+            },
+          },
+        ],
+      },
+    ],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'nunjucks-html-loader!./src/templates/index.njk',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
 }

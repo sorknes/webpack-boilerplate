aa7169f0c01f22ecc7066a36ab16b31bb84d2603
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-02-25 12:46:34 +0100

    Update webpack.config.common.js

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -4,4 +4,14 @@
 // ===============================================================================
 //
 
-module.exports = {}
+let src = `./src`
+let styles = `${src}/styles/scss`
+let scripts = `${src}/scripts`
+
+module.exports = {
+  entry: [`${scripts}/main.js`, `${styles}/styles.scss`],
+
+  module: {
+    rules: [{}],
+  },
+}

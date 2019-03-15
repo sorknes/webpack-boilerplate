dad839b8bb80cf84bfb8d1b9991cac0efbace635
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-03-15 11:07:54 +0100

    Update webpack.config.common.js

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -6,17 +6,64 @@
 
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const SvgSpriteHtmlWebpackPlugin = require('svg-sprite-html-webpack')
+const glob_entries = require('webpack-glob-folder-entries')
 
 const PATHS = {
-  src: path.join(__dirname, '/src'),
-  styles: path.join(__dirname, '/src/assets/styles/scss'),
-  images: path.join(__dirname, '/src/assets/images'),
-  scripts: path.join(__dirname, '/src/assets/scripts'),
-  templates: '/src/templates',
+  src: './src',
+  styles: './src/assets/styles/scss',
+  images: './src/assets/images',
+  scripts: './src/assets/scripts',
+  templates: './src/templates',
+}
+
+function returnEntries(globPath) {
+  let entries = glob_entries(globPath, true)
+  let folderList = new Array()
+  for (let folder in entries) {
+    folderList.push(path.join(__dirname, entries[folder]))
+  }
+  return folderList
 }
 
 module.exports = {
   entry: [`${PATHS.scripts}/main.js`, `${PATHS.styles}/wb-styles.scss`],
 
-  plugins: [new HtmlWebpackPlugin({ template: `${PATHS.src}/index.html` })],
+  module: {
+    rules: [
+      {
+        test: /\.html$|njk|nunjucks/,
+        use: [
+          { loader: 'html-loader', options: { minimize: true } },
+          {
+            loader: 'nunjucks-html-loader',
+            options: {
+              searchPaths: [...returnEntries(`${PATHS.src}/**/`)],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        exclude: /node_modules/,
+        use: 'svg-url-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `nunjucks-html-loader!${PATHS.src}/index.njk`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new HtmlWebpackPlugin({
+      template: `nunjucks-html-loader!${PATHS.src}/about.njk`,
+      filename: 'about.html',
+      inject: 'body',
+    }),
+    new SvgSpriteHtmlWebpackPlugin({
+      includeFiles: [`${PATHS.images}/icons/*.svg`],
+    }),
+  ],
 }
